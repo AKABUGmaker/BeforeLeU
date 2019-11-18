d0a193b590a68f1ce78da23d1fa931129dd9f825
xiaoFan <775069279@qq.com>
2019-11-18 21:35:18 +0800

    springboot(未完成)

---
+++ src/main/java/cn/itcast/springboot/JdbcConfig.java
@@ -0,0 +1,27 @@
+package cn.itcast.springboot;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableConfigurationProperties(JdbcProperties.class)
+public class JdbcConfig {
+
+    @Autowired
+    JdbcProperties properties;
+
+    @Bean
+    public DataSource getDataSource(){
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl(properties.getUrl());
+        dataSource.setDriverClassName(properties.getDriverClassName());
+        dataSource.setUsername(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+        return dataSource;
+    }
+}
