1fd5a41f24f7293e54ceef7a1756c70841b448f3
xiaoFan <775069279@qq.com>
2019-11-25 22:04:13 +0800

    乐优项目之前的学习内容(springboot,lucene,es,springCloud第一天内容已完成,第二天未完成)

--- cloud-demo/user-consuer/src/main/java/cn/itcast/demo/ConsumerApplication.java
+++ cloud-demo/user-consuer/src/main/java/cn/itcast/demo/ConsumerApplication.java
@@ -4,11 +4,14 @@ package cn.itcast.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 public class ConsumerApplication {
 
     public static void main(String[] args) {
@@ -17,7 +20,8 @@ public class ConsumerApplication {
 
 
     @Bean
-    public RestTemplate getRestTemplate(){
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
 }
--- cloud-demo/user-consuer/src/main/java/cn/itcast/demo/controller/UserController.java
+++ cloud-demo/user-consuer/src/main/java/cn/itcast/demo/controller/UserController.java
@@ -1,6 +1,7 @@
 package cn.itcast.demo.controller;
 
 import cn.itcast.demo.pojo.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,21 +24,48 @@ public class UserController {
     private DiscoveryClient discoveryClient;
 
     @GetMapping("{id}")
+    @HystrixCommand(fallbackMethod = "queryUserByIdFallBack")
     public User queryUserById(@PathVariable("id")Long id){
 
-        //根据服务id获取服务实例集合
-        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
+//        //根据服务id获取服务实例集合
+//        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
+//
+//        ServiceInstance serviceInstance = instances.get(0);
+//
+//        //主机地址
+//        String host = serviceInstance.getHost();
+//
+//        int port = serviceInstance.getPort();
 
-        ServiceInstance serviceInstance = instances.get(0);
+        //测试熔断
+        if (1==id){
+            throw new RuntimeException("自定义一个异常");
+        }
+        /**
+         * 请求服务地址时,需要将user-service转换为ip+sort
+         * 获取ip和sort需要前往注册表,如果部署了集群
+         * 需要通过*ribbon*来选取user-service的地址,
+         * 实现负载均衡
+         */
+        String url = "http://user-service/user/hello/" + id;
 
-        //主机地址
-        String host = serviceInstance.getHost();
-
-        int port = serviceInstance.getPort();
+        User user = restTemplate.getForObject(url,User.class);
 
 
-        User user = restTemplate.getForObject("http://"+host+":"+port+"/user/hello/" + id, User.class);
+        return user;
 
+    }
+
+    /**
+     * 回调方法的返回值类型，和参数类型，必须保持一致
+     * @param id
+     * @return
+     */
+    public User queryUserByIdFallBack(@PathVariable("id")Long id){
+
+        User user = new User();
+        user.setId(id);
+        user.setNote("小姐姐说，明天再来接待你哦");
 
         return user;
 
